6f2f19a7aa6ebdd937a0616f9778d6d1d7613e4c
Mikhail Nosov <manosov1984@gmail.com>
2022-01-08 00:21:08 +0400

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  const newN = n.toString().split('').reverse().join('');
+  return parseFloat(newN);
 }
+
+
+// function reverse (n) {
+//   const newN = n.toString().split('').reverse().join('')
+// console.log(parseFloat(newN))
+// }
+// reverse(10)
